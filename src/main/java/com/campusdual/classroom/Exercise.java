dee6f644f2c09cc3523b23cf1c3fd2497257e496
pablosandovalpita <pablo.sandoval@campusdual.com>
2025-10-26 21:26:32 +0100

    7

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -5,7 +5,7 @@ public class Exercise {
     public static void main(String[] args) {
         System.out.println("Ejemplos de cálculos SIN estructuras de control:");
         System.out.println("Suma de los primeros 5 números: " + (1 + 2 + 3 + 4 + 5));
-        System.out.println("Suma de los primeros 20 números pares: " + (2 + 4 + 6 + 8 + 10 + 12 + 14 + 16 + 18 + 20 + 22 + 24 + 26 + 28 + 30 + 32 + 34 + 36+ 38 + 40));
+        System.out.println("Suma de los primeros 20 números pares: " + (2 + 4 + 6 + 8 + 10 + 12 + 14 + 16 + 18 + 20 + 22 + 24 + 26 + 28 + 30 + 32 + 34 + 36 + 38 + 40));
         System.out.println("Obtén el factorial del número 7 → (7!): " + (7 * 6 * 5 * 4 * 3 * 2 * 1));
         System.out.println("######################################################################");
         System.out.println("Ejemplos de cálculos CON estructuras de control:");
@@ -16,18 +16,38 @@ public class Exercise {
     }
 
     public static int sumNum(int num) {
-        return 0;
+        int resultado = 0;
+        for (int n = 1; n <= num; n++) {
+            resultado += n;
+        }
+        return resultado;
     }
 
     public static int sumEvenNum(int num) {
-        return 0;
+        int resultado = 0;
+        int contador = 0;
+        int actual = 2;
+
+        while (contador < num) {
+            resultado += actual;
+            actual += 2;
+            contador++;
+        }
+        return resultado;
     }
 
     public static int factorial(int num) {
-        return 0;
+        int producto = 1;
+        for (int i = num; i > 1; i--) {
+            producto *= i;
+        }
+        return producto;
     }
 
     public static int recursiveFactorial(int num) {
-        return 0;
+        if (num == 1) {
+            return 1;
+        }
+        return num * recursiveFactorial(num - 1);
     }
 }
